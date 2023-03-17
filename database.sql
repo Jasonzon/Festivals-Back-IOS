a05be94d1c814574fd6d117e79bbbac6794502e2
Jason <jazzmoret@gmail.com>
2023-03-17 11:59:37 +0100

    update DB

--- database.sql
+++ database.sql
@@ -1,13 +1,37 @@
 create database festivals;
 
+create table festival(
+    festival_id serial primary key,
+    festival_name varchar(255) not null,
+    festival_year varchar(255) not null,
+    festival_days int not null,
+    festival_open boolean not null
+    festival_debut date not null,
+    festival_fin date not null
+);
+
+create table jour(
+    jour_id serial primary key,
+    jour_name varchar(255) not null,
+    jour_debut time not null,
+    jour_fin time not null,
+    jour_date date not null,
+    jour_festival int not null,
+    foreign key (jour_festival) references festival(festival_id) on delete cascade
+);
+
+create type user_role as enum('Admin','Basic');
+
 create table benevole(
     benevole_id serial primary key,
     benevole_prenom varchar(255) not null,
     benevole_nom varchar(255) not null,
-    benevole_mail varchar(255) unique not null
+    benevole_mail varchar(255) unique not null,
+    polyuser_password varchar(255) not null,
+    polyuser_role user_role default 'Basic'
 );
 
-create type type_jeu as enum ('enfant','famille','ambiance','initie','expert');
+create type type_jeu as enum ('Enfant','Famille','Ambiance','Initie','Expert');
 
 create table jeu(
     jeu_id serial primary key,
@@ -17,7 +41,10 @@ create table jeu(
 
 create table zone(
     zone_id serial primary key,
-    zone_name varchar(255) not null
+    zone_name varchar(255) not null,
+    zone_benevoles int not null,
+    zone_festival int not null,
+    foreign key (zone_festival) references festival(festival_id) on delete cascade
 );
 
 create table affectation(
@@ -30,8 +57,10 @@ create table affectation(
 
 create table creneau(
     creneau_id serial primary key,
-    creneau_debut timestamp not null,
-    creneau_fin timestamp not null
+    creneau_debut time not null,
+    creneau_fin time not null,
+    creneau_jour int not null,
+    foreign key (creneau_jour) references jour(jour_id) on delete cascade
 );
 
 create table travail(
@@ -42,15 +71,4 @@ create table travail(
     foreign key (travail_zone) references zone(zone_id) on delete cascade,
     travail_creneau int not null,
     foreign key (travail_creneau) references creneau(creneau_id) on delete cascade
-);
-
-create type user_role as enum('admin','basic');
-
-create table polyuser(
-    polyuser_id serial primary key,
-    polyuser_mail varchar(255) unique not null,
-    polyuser_nom varchar(255) not null,
-    polyuser_prenom varchar(255) not null,
-    polyuser_password varchar(255) not null,
-    polyuser_role user_role default 'basic'
 );
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const PORT = process.env.PORT || 5000
-const path = require('path');
 
 app.use(cors())
 app.use(express.json())
@@ -19,13 +18,9 @@ app.use("/travail", require("./routes/travail"))
 
 app.use("/creneau", require("./routes/creneau"))
 
-app.use("/polyuser", require("./routes/polyuser"))
+app.use("/festival", require("./routes/festival"))
 
-/*
-app.get("/*", (req,res) => {
-    res.sendFile(path.join(__dirname,"client/build/index.html"))
-})
-*/
+app.use("/jour", require("./routes/jour"))
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
