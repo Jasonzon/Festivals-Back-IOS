7b509a73f2e0d2be71fd380c63c17eb547a19e47
Jason <jazzmoret@gmail.com>
2023-03-27 15:32:32 +0200

    update route GET

--- routes/benevole.js
+++ routes/benevole.js
@@ -16,7 +16,7 @@ router.get("/", async (req,res) => {
     }
 })
 
-router.get("/:id", async (req,res) => {
+router.get("/id/:id", async (req,res) => {
     try {
         const {id} = req.params
         console.log(`GET /benevole/${id}`)
